41a73bd9bccf3afdba839de8fef8e307d11040db
Komarovskiy David <51922675+Davidyshka@users.noreply.github.com>
2023-09-27 02:00:00 +0300

    task6

---
+++ Task6#1/index.js
@@ -0,0 +1,7 @@
+let h1=document.getElementById('h1')
+let pText=document.getElementById('text')
+let li=document.getElementById('listItem3')
+let cLi=document.getElementsByTagName('list-item');
+
+console.log(`${h1} \n ${pText} \n ${li} \n ${cLi}`)
+
