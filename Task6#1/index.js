bf921444af0c6511ecd2253c39ca9da8d055f765
Komarovskiy David <51922675+Davidyshka@users.noreply.github.com>
2023-09-27 02:22:57 +0300

    task6.4

--- Task6#1/index.js
+++ Task6#1/index.js
@@ -1,7 +1,13 @@
-let h1=document.getElementById('h1')
+let mainH1=document.getElementById('main-text')
 let pText=document.getElementById('text')
 let li=document.getElementById('listItem3')
 let cLi=document.getElementsByTagName('list-item');
 
-console.log(`${h1} \n ${pText} \n ${li} \n ${cLi}`)
+console.log(` ${mainH1} \n ${pText} \n ${li} \n ${cLi}`);
 
+
+console.log(`${mainH1.outerText}`)
+mainH1.innerHTML='Title level 1'
+console.log(mainH1.innerHTML)
+
+li.innerHTML = '<strong>List item 3</strong>'
